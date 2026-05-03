79527136bfe1e41ad6bdc41e9bb78ac152dd17f0
develeacid <ele.leader@gmail.com>
2026-05-02 18:14:14 -0600

    feat(transparencia): conexiones pgsql_public y pgsql_public_read

--- config/database.php
+++ config/database.php
@@ -99,6 +99,36 @@ return [
             'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
 
+        'pgsql_public' => [
+            'driver' => 'pgsql',
+            'url' => env('DB_PUBLIC_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_PUBLIC_DATABASE', 'spp_public'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+        ],
+
+        'pgsql_public_read' => [
+            'driver' => 'pgsql',
+            'url' => env('DB_PUBLIC_READ_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_PUBLIC_DATABASE', 'spp_public'),
+            'username' => env('DB_PUBLIC_PORTAL_USER', 'spp_portal'),
+            'password' => env('DB_PUBLIC_PORTAL_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+        ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
